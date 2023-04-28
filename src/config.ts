9211e09bd306f23b0a8c9c7719c11322ac4fa16f
doctorsolana <127446248+doctorsolana@users.noreply.github.com>
2023-04-28 18:22:47 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export type Config = ReturnType<typeof getConfig>
 export const getConfig = () => {
   const conf = import.meta.env
   return {
-    appName: conf.GAMBA_APP_NAME as string | undefined,
-    appLink: conf.GAMBA_APP_LINK as string | undefined,
+    appName: "https://twitter.com/DeFlipSOL",
+    appLink: "https://twitter.com/DeFlipSOL",
     creatorAddress: conf.GAMBA_CREATOR_ADDRESS as string | undefined,
     rpcEndpoint: conf.GAMBA_SOLANA_RPC as string | undefined,
     rpcWsEndpoint: conf.GAMBA_SOLANA_RPC_WS as string | undefined,
